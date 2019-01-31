8fdd7e8b4868d93802a595fe615740078da17156
bzk <bb10@williams.edu>
2019-01-30 23:53:44 -0500

    Formalized 1st Metzlerian Expansion
    Added Solow style Cobb-Dogulas production function to Metzlerian model

--- computational/metzlerian_cobb-douglas/timeseries.py
+++ computational/metzlerian_cobb-douglas/timeseries.py
@@ -19,11 +19,11 @@ Model assumes that government budget is counter-cyclical (relatively realistic)
 
 #Define income as the sum of production outputs.
 def metzlerian(I, S, U, G):
-    Y = I + S + U + G
+    Y = I + S + U 
     return Y
 
 '''
-Solves for government expenditure. Expenditure increases during recessions and increases during booms.
+for government expenditure. Expenditure increases during recessions and increases during booms.
 Current equation for expenditure is naively created, will be updated with more research
 Concave and decreasing function
 '''
@@ -31,40 +31,53 @@ def government(g, Y):
     G = g / Y
     return G
 
-#Solves for consumption in terms of income and taxes
+#Consumption in terms of income and taxes
 def consumption(b, Y, T):
     C = b * (Y - T)
     return C
 
-#Solves for heterogenous expectation weighting based on steady state income
+#Heterogenous expectation weighting based on steady state income
 def weight(Cbar, Clag)
     w = 1 / (1 + d * (Cbar - Clag) )
     return w
 
-#Solves expected consumption as a weighted average
-def averageexpec(Ue, Ur, w):
+#Expected consumption as a weighted average
+def averageexpec(Cbar, Clag, f, c, d):
+    Ur = Clag + f * (Cbar - Clag)
+    Ue = Clag + c * (Clag - Cbar)
+    w = 1 / (1 + d * (Cbar - Clag) 
     U = w * Ue + (1 - w) * Ur
     return U
 
-#Solves regressive expectation outcome
-def rexpec(Cbar, Clag, f):
-    Ur = Clag + f * (Cbar - Clag)
-    return Ur
-
-#Solves extrapolative expectation outcome
-def eexpec(Cbar, Clag, c):
-    Clag + c * (Clag - Cbar)
-    return Ue
-
-#Solves desired level of inventory
+#Desired level of inventory
 def desinventory(k, U)
     Qhat = k * U
     return Qhat
 
-#Solves actual level of inventory
+#Actual level of inventory
 def actinventory(Qhat, C, U)
     Q = Qhat - (C - U)
 
+#Predicted income level 
+def predinc(Ylag, Ybar, h, j, p)
+    Pr = Ylag + j * (Ybar - Ylag)
+    Pe = Ylag + h * (Ylag - Ybar)
+    v = 1 / (1 + p * (Ybar - Ylag) )
+    P = v * Pe + (1 - v) * Pr
+    return P
 
+#Current capital
+def capital(Ilag, delta, Klag):
+    K = Ilag + (1 - delta) * Klag
+    return K
+
+#Investment 
+def invest(Pfut, L, Klag, alpha, delta):
+    I = ((Pfut)(L ** (alpha - 1))) ** alpha - (1 - delta) ** Klag
+    return I
+
+#Solves for timeseries plot
+def timeseries():
+    
 
 
