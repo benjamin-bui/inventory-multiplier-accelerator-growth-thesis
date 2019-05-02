757d61eb4cc5707eeba31c87b279d80549ecab46
bzk <bb10@williams.edu>
2019-05-01 22:53:57 -0400

    2nd major draft of presentation and paper

--- computational/other/brownian.py
+++ computational/other/brownian.py
@@ -2,13 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def timeseries(mean, sigma, iterations):
-    position = sigma * np.random.randn(iterations, 1) + mean
-    for i in range(iterations):
-        position[i] = position[i-1] + position[i]
     #Time series of mapping
     fig = plt.figure(dpi=1200)
     ax1 = fig.add_subplot(1, 1, 1)
-    ax1.plot(position, c='blue', linewidth=0.5)
+    for j in range(10):
+        position = sigma * np.random.randn(iterations, 1) + mean
+        position[0] = 0
+        for i in range(1, iterations):
+            position[i] = position[i-1] + position[i]
+        ax1.plot(position, c='blue', linewidth=0.5)
     plt.ylabel('y(t)')
     plt.xlabel('t')
 timeseries(0, 1, 100)
